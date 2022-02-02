894ca184a07dfe2ce8d6ce4bb465b629cbb88dd4
JHunter09 <9.j.hunter@gmail.com>
2022-02-01 23:57:20 -0500

    Removed link to webpage's individual project page - Unneeded as of now

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -50,16 +50,15 @@ export default function Homepage() {
           <h2 class="mb-2">Some of Our Work</h2>
           <div class="row gap-2">
             <div class="col-12-xs col-6-md col-3-lg">
-              <Link to="/projects/webpage">
-                <div class="card p-0">
-                  <h3 class="card-title m-1">
-                    Webpage{" "}
-                    <span class="badge-orange text-white ml-1">new</span>
-                  </h3>
-                  <img src="/img/webpage.png" alt="" />
-                  <p class="m-1">This very website! Made with ReactJS</p>
-                </div>
-              </Link>
+              {/* <Link to="/projects/webpage"> */}
+              <div class="card p-0">
+                <h3 class="card-title m-1">
+                  Webpage <span class="badge-orange text-white ml-1">new</span>
+                </h3>
+                <img src="/img/webpage.png" alt="" />
+                <p class="m-1">This very website! Made with ReactJS</p>
+              </div>
+              {/* </Link> */}
             </div>
             <div class="col-12-xs col-6-md col-3-lg">
               <div class="card p-0">
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,16 +9,15 @@ export default function Projects() {
           <h2 class="mb-2">Our Projects</h2>
           <div class="row gap-2">
             <div class="col-12-xs col-6-md col-3-lg">
-              <Link to="/projects/webpage">
-                <div class="card p-0">
-                  <h3 class="card-title m-1">
-                    Webpage{" "}
-                    <span class="badge-orange text-white ml-1">new</span>
-                  </h3>
-                  <img src="/img/webpage.png" alt="" />
-                  <p class="m-1">This very website! Made with ReactJS</p>
-                </div>
-              </Link>
+              {/* <Link to="/projects/webpage"> */}
+              <div class="card p-0">
+                <h3 class="card-title m-1">
+                  Webpage <span class="badge-orange text-white ml-1">new</span>
+                </h3>
+                <img src="/img/webpage.png" alt="" />
+                <p class="m-1">This very website! Made with ReactJS</p>
+              </div>
+              {/* </Link> */}
             </div>
           </div>
         </div>
