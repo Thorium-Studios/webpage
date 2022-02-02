6c6928182d90ef6cc3aae1328ad910e498bdbaed
JHunter09 <9.j.hunter@gmail.com>
2022-02-01 22:36:21 -0500

    populated Projects.js with cards from homepage

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Projects() {
   return (
     <div>
-      <p>Current Projects:</p>
+      <section id="projects" class="mt-5">
+        <div class="container">
+          <h2 class="mb-2">Our Projects</h2>
+          <div class="row gap-2">
+            <div class="col-12-xs col-6-md col-3-lg">
+              <Link to="/projects/webpage">
+                <div class="card p-0">
+                  <h3 class="card-title m-1">
+                    Webpage{" "}
+                    <span class="badge-orange text-white ml-1">new</span>
+                  </h3>
+                  <img src="/img/webpage.png" alt="" />
+                  <p class="m-1">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Totam, hic!
+                  </p>
+                </div>
+              </Link>
+            </div>
+            <div class="col-12-xs col-6-md col-3-lg">
+              <div class="card p-0">
+                <h3 class="card-title m-1">Thorium Recipes</h3>
+                <img src="/img/food.png" alt="" />
+                <p class="m-1">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Totam, hic!
+                </p>
+              </div>
+            </div>
+            <div class="col-12-xs col-6-md col-3-lg">
+              <div class="card p-0">
+                <h3 class="card-title m-1">Mario Club</h3>
+                <img src="/img/mario.png" alt="" />
+                <p class="m-1">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Totam, hic!
+                </p>
+              </div>
+            </div>
+            <div class="col-12-xs col-6-md col-3-lg">
+              <div class="card p-0">
+                <h3 class="card-title m-1">Thorium Things</h3>
+                <img src="/img/notes.png" alt="" />
+                <p class="m-1">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Totam, hic!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
