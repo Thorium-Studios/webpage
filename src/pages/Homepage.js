e54a426b93a204bbea4aa6272da96f7cb3841903
JHunter09 <9.j.hunter@gmail.com>
2022-02-02 20:30:44 -0500

    Updated first project to VTAC website

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -51,13 +51,16 @@ export default function Homepage() {
           <div class="row gap-2">
             <div class="col-12-xs col-6-md col-3-lg">
               {/* <Link to="/projects/webpage"> */}
-              <div class="card p-0">
-                <h3 class="card-title m-1">
-                  Webpage <span class="badge-orange text-white ml-1">new</span>
-                </h3>
-                <img src="/img/webpage.png" alt="" />
-                <p class="m-1">This very website! Made with ReactJS</p>
-              </div>
+              <a href="https://vikingtactical.us">
+                <div class="card p-0">
+                  <h3 class="card-title m-1">
+                    VTAC Website{" "}
+                    <span class="badge-orange text-white ml-1">new</span>
+                  </h3>
+                  <img src="/img/vtac.png" alt="" />
+                  <p class="m-1">VikingTactical's clan website</p>
+                </div>
+              </a>
               {/* </Link> */}
             </div>
             <div class="col-12-xs col-6-md col-3-lg">
